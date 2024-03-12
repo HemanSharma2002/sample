f73c3351b57926700ae8ac9b75df85a9d738c726
Heman Sharma <hemansharma2002@gmail.com>
2024-03-12 10:11:13 +0530

    1st

---
+++ sample/src/App.jsx
@@ -0,0 +1,12 @@
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+function App() {
+  return (
+    <>
+    <div className='flex flex-row justify-center pt-10'>Hello
+      <AcUnitIcon/>
+    </div>
+    </>
+  )
+}
+
+export default App
